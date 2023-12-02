489ee278f68223a3098fb3321e8f363ef513b75c
Varun Tandon <varuntandon28121993@gmail.com>
2023-12-03 00:28:14 +0530

    chore: add compose deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,20 +24,40 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
   }
+
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
   }
+
   kotlinOptions {
     jvmTarget = "1.8"
+  }
+
+  buildFeatures {
+    compose = true
+  }
+
+  composeOptions {
+    kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExtensionVersion.get()
   }
 }
 
 dependencies {
+  val composeBom = platform(libs.androidx.compose.bom)
+  implementation(composeBom)
+  androidTestImplementation(composeBom)
 
   implementation(libs.core.ktx)
   implementation(libs.appcompat)
   implementation(libs.material)
+
+  // Material Design 3
+  implementation(libs.androidx.compose.material3)
+
+  // Android Studio Preview support
+  implementation(libs.androidx.compose.ui.tooling.preview)
+  debugImplementation(libs.androidx.compose.ui.tooling)
 
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.test.ext.junit)
